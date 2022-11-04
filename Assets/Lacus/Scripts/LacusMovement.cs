76dee1da4a8c2f060c02931481d872f37091f881
m0ki <miquel.forcada@enti.cat>
2022-11-04 18:03:10 +0100

    Updated Lacus Movement
    Changed Forward Function
    Changed Rotate Function
    Merging into develop to get a function to detect when Lacus crosses tiles

--- Assets/Lacus/Scripts/LacusMovement.cs
+++ Assets/Lacus/Scripts/LacusMovement.cs
@@ -8,11 +8,13 @@ public class LacusMovement : MonoBehaviour
 {
     public Transform globalRotation;
     private Vector3 desiredRotation;
+    public LacusStats lacusStats;
 
     // Start is called before the first frame update
     void Start()
     {
         desiredRotation = new Vector3(0f, 0f, 90f);
+        //Rotate(desiredRotation);
     }
 
     // Update is called once per frame
@@ -31,9 +33,15 @@ public class LacusMovement : MonoBehaviour
     // Funció que fa avançar en Lacus en direcció X
     void Forward()
     {
+       
         // Limitar el moviment del Lacus (Implementar vida/bateria)
         // Moure's amb DoTween
-        transform.DOLocalMoveX(transform.localPosition.x + 1f, 6f, false);
+        if (lacusStats.batteryLeft > 0)
+        {
+            //transform.DOLocalMoveX(transform.localPosition.x + lacusStats.batteryLeft, 4, false);
+            transform.DOLocalMoveX(transform.localPosition.x + 1f, 1f, false);
+
+        }
 
         // Moure's amb Unity
         //transform.localPosition = transform.localPosition + new Vector3(1f * Time.deltaTime, 0f, 0f);
@@ -42,14 +50,12 @@ public class LacusMovement : MonoBehaviour
     // Funció que rota en Lacus donat una rotació amb un valor Z
     void Rotate(Vector3 rotation)
     {
-        Vector3 lacusDirection = new Vector3(0f, 0f, globalRotation.transform.localRotation.z);
+        Vector3 lacusDirection = new Vector3(0f, 0f, rotation.z);
 
         // Rotar amb DoTween
-        globalRotation.transform.DOLocalRotate(lacusDirection + rotation, 0.5f, RotateMode.Fast);
+        globalRotation.transform.DOLocalRotate(rotation, 0.5f, RotateMode.Fast);
 
         // Rotar amb Unity
         //rotationValues.transform.rotation = Quaternion.Euler(lacusDirection + rotation);
     }
-
-
 }
