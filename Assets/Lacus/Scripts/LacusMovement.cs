a7c05684935f628f68539c51967da9d5efa9a3d3
m0ki <miquel.forcada@enti.cat>
2022-11-08 11:52:19 +0100

    Finished Lacus' Movement
    Lacus' Movement is almost completed, it only remains the movement limitation (Battery) and its auto movement.
    Fixed a bug that caused Lacus to rotate when he was next to the arrow tile
    Added the Stop tag

--- Assets/Lacus/Scripts/LacusMovement.cs
+++ Assets/Lacus/Scripts/LacusMovement.cs
@@ -6,9 +6,9 @@ using System.Security.Cryptography;
 
 public class LacusMovement : MonoBehaviour
 {
-    public Transform lacusParent;
-    private Vector3 desiredRotation;
+    //public Transform lacusParent;
     public LacusStats lacusStats;
+    public Transform destination;
 
 
     // Start is called before the first frame update
@@ -25,28 +25,19 @@ public class LacusMovement : MonoBehaviour
     // Funció que fa avançar en Lacus en direcció X
     void Forward()
     {
-
         // Limitar el moviment del Lacus (Implementar vida/bateria)
         // Moure's amb DoTween
         if (lacusStats.batteryLeft > 0 && Input.GetKeyDown(KeyCode.Space))
         {
-            //transform.DOLocalMoveX(transform.localPosition.x + lacusStats.batteryLeft, 4, false);
-            //lacusParent.transform.DOMoveX(lacusParent.transform.localPosition.x + 1f, 1f, false);
-            transform.DOLocalMoveX(lacusParent.transform.position.x + 1f, 1f, false);
+            transform.DOLocalMoveX(destination.transform.position.x, 1f, false);
+            transform.DOLocalMoveY(destination.transform.position.y, 1f, false);
         }
-
-        // Moure's amb Unity
-        //transform.localPosition = transform.localPosition + new Vector3(1f * Time.deltaTime, 0f, 0f);
     }
 
     // Funció que rota en Lacus donat una rotació amb un valor Z
     public void Rotate(Quaternion rotation)
     {
-
         // Rotar amb DoTween
         transform.DORotateQuaternion(rotation, 0.5f);
-
-        // Rotar amb Unity
-        //rotationValues.transform.rotation = Quaternion.Euler(lacusDirection + rotation);
     }
 }
