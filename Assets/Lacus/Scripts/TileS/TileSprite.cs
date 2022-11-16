f23fe76b6c5707a48adaea97584481fa7fbda43e
m0ki <miquel.forcada@enti.cat>
2022-11-16 18:12:57 +0100

    Arrow Code Refactor And Separated TileHover function
    Changed the Arrow Rotation Code
    Separated the over function of the tile

--- Assets/Lacus/Scripts/TileS/TileSprite.cs
+++ Assets/Lacus/Scripts/TileS/TileSprite.cs
@@ -9,22 +9,10 @@ public class TileSprite: MonoBehaviour
     public Color color1;
     public Color color2;
     public SpriteRenderer spriteRenderer;
-    public GameObject highlight;
-
 
     public void Init(bool isOffset)
     {
         spriteRenderer.color = isOffset ? color1 : color2;
-    }
-
-    private void OnMouseEnter()
-    {
-        highlight.SetActive(true);
-    }
-
-    private void OnMouseExit()
-    {
-        highlight.SetActive(false);
     }
 }
 
