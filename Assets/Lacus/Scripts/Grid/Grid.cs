356b855558db9edbda774b5e1bc142533e2cfe3f
m0ki <miquel.forcada@enti.cat>
2022-11-24 13:06:00 +0100

    Added Walls Layout, Adjusted All Tiles position and Scale
    This walls layout only works on 7x9 layout because it's hardcoded. (Modify this to make smaller levels or irregular levels)
    Also modified all the tiles scale and position to adjust them to the screen, before this they were too large to fit them on the screen.

--- Assets/Lacus/Scripts/Grid/Grid.cs
+++ Assets/Lacus/Scripts/Grid/Grid.cs
@@ -14,19 +14,21 @@ public class Grid : MonoBehaviour
     public void GenerateGrid()
     {
         tiles = new Dictionary<Vector2, TileSprite>();
-        for (int x = 0; x < width; x++)
+        float leftMargin = 0.4f; // Alineació horitzontal
+        float topMargin = -0.55f; // Alineació vertical
+
+        for (int x = 1; x < width+1; x++)
         {
-            for (int y = 0; y > -height; y--)
+            for (int y = -1; y > -height-1; y--)
             {
-                TileSprite spawnedTile = Instantiate(tile, new Vector3(x, y), Quaternion.identity);
+                TileSprite spawnedTile = Instantiate(tile, new Vector3(x - leftMargin - x * (1 - tile.transform.localScale.x), y - topMargin - y * (1 - tile.transform.localScale.y)), Quaternion.identity);
                 
                 spawnedTile.name = $"Tile {x} {y}";
 
                 bool isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
 
-                tiles[new Vector2(x, y)] = spawnedTile;
-            }
+                tiles[new Vector2(x - tile.transform.localScale.x, y + tile.transform.localScale.y)] = spawnedTile;            }
         }
         worldCoords.transform.position = new Vector3(2, -3, -10);
     }
--- Assets/Lacus/Scripts/Grid/ReadLevelFile.cs
+++ Assets/Lacus/Scripts/Grid/ReadLevelFile.cs
@@ -2,20 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Windows;
 
 public class ReadLevelFile : MonoBehaviour
 {
+    [SerializeField] private string levelName;
+
     [SerializeField] private GameObject arrow;
-    [SerializeField] private GameObject normalTile;
     [SerializeField] private GameObject battery;
     [SerializeField] private GameObject lacus;
     [SerializeField] private GameObject button;
-    [SerializeField] private GameObject exit;
+    [SerializeField] private GameObject final;
     [SerializeField] private GameObject stop;
-    [SerializeField] private GameObject wall;
-    [SerializeField] private string levelName;
+
+    // Walls corner
+    [SerializeField] private GameObject TopLeftWall;
+    [SerializeField] private GameObject TopRightWall;
+    [SerializeField] private GameObject BotLeftWall;
+    [SerializeField] private GameObject BotRightWall;
+
+    [SerializeField] private GameObject TopWall;
+    [SerializeField] private GameObject BotWall;
+    [SerializeField] private GameObject LeftWall;
+    [SerializeField] private GameObject RightWall;
+
 
 
     // Start is called before the first frame update
@@ -34,6 +46,13 @@ public class ReadLevelFile : MonoBehaviour
     {
         float x = 0f;
         float y = 0f;
+        float leftMargin = 0.4f; // Alineació horitzontal
+        float topMargin = -0.55f; // Alineació vertical
+
+        GameObject sprite = TopLeftWall;
+
+    
+
         string line = "";
 
         try
@@ -48,58 +67,102 @@ public class ReadLevelFile : MonoBehaviour
 
         foreach (char tile in line)
         {
-            /*if (tile != ' ')
-            {
-                Debug.Log(tile);
-            }*/
-
             switch (tile)
             {
                 case '<': // Arrow
                     {
-                        Instantiate(arrow, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(arrow, new Vector3(x - leftMargin - x * (1 - sprite.transform.localScale.x), y - topMargin - y * (1 - sprite.transform.localScale.y), 0), Quaternion.identity);
                         break;
                     }
                 case '_': // Normal tile
                     {
-                        //Instantiate(normalTile, new Vector3(x, y, 0), Quaternion.identity);
                         break;
                     }
                 case 'B': // Battery
                     {
-                        Instantiate(battery, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(battery, new Vector3(x - leftMargin - x * (1 - sprite.transform.localScale.x), y - topMargin - y * (1 - sprite.transform.localScale.y), 0), Quaternion.identity);
                         break;
                     }
                 case 'L': // Lacus
                     {
-                        Instantiate(lacus, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(lacus, new Vector3(x - leftMargin - x * (1 - sprite.transform.localScale.x), y - topMargin - y * (1 - sprite.transform.localScale.y), 0), Quaternion.identity);
                         break;
                     }
                 case '?': // Button
                     {
-                        Instantiate(button, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(button, new Vector3(x - leftMargin - x * (1 - sprite.transform.localScale.x), y - topMargin - y * (1 - sprite.transform.localScale.y), 0), Quaternion.identity);
                         break;
                     }
                 case '!': // Deactivated Arrow
                     {
-                        Instantiate(arrow, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(arrow, new Vector3(x - leftMargin - x * (1 - sprite.transform.localScale.x), y - topMargin - y * (1 - sprite.transform.localScale.y), 0), Quaternion.identity);
                         break;
                     }
-                case 'E': // Exit
+                case 'F': // Exit
                     {
-                        Instantiate(exit, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(final, new Vector3(x - leftMargin - x * (1 - sprite.transform.localScale.x), y - topMargin - y * (1 - sprite.transform.localScale.y), 0), Quaternion.identity);
                         break;
                     }
                 case 'S': // Stop
                     {
-                        Instantiate(stop, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(stop, new Vector3(x - leftMargin - x * (1 - sprite.transform.localScale.x), y - topMargin - y * (1 - sprite.transform.localScale.y), 0), Quaternion.identity);
                         break;
                     }
                 case '#': // Wall
                     {
-                        Instantiate(wall, new Vector3(x, y, 0), Quaternion.identity);
+                        // Problemes,
+                        // buscar una solució al hardcode de la ultima casella de la fila i la ultima fila,
+                        // es a dir buscar una alternativa a x == 4 i y == 6
+
+                        if (x == 0 && y == 0)
+                        {
+                            // Corner Top Left
+                            sprite = TopLeftWall;
+                        }
+                        else if (x == 6 && y == 0)
+                        {
+                            // Corner Top Right
+                            sprite = TopRightWall;
+                        }
+                        else if (x == 0 && -y == 8) 
+                        {
+                            // Corner Bot Left
+                            sprite = BotLeftWall;
+                        }
+                        else if (x == 6 && -y == 8)
+                        {
+                            // Corner Bot Right
+                            sprite = BotRightWall;
+                        }
+                        else if (y == 0)
+                        {
+                            // Walls top
+                            sprite = TopWall;
+                        }
+                        else if (y == -8)
+                        {
+                            // Walls Bot
+                            sprite = BotWall;
+                        }
+                        else if (x == 0)
+                        {
+                            // Walls Left
+                            sprite = LeftWall;
+                        }
+                        else if (x == 6)
+                        {
+                            // Walls Right
+                            sprite = RightWall;
+                        }
+                        else
+                        {
+                            Debug.LogError("Walls exteriors no correctes");
+                        }
+                        Instantiate(sprite, new Vector3(x - leftMargin - x * (1 - sprite.transform.localScale.x), y - topMargin - y * (1 - sprite.transform.localScale.y), 0), Quaternion.identity);
+
                         break;
                     }
+
 
                 // En el cas de fer més caselles, seguir la estructura
 
@@ -127,3 +190,98 @@ public class ReadLevelFile : MonoBehaviour
         }
     }
 }
+
+/*
+  switch (tile)
+            {
+                case '<': // Arrow
+                    {
+                        Instantiate(arrow, new Vector3(x - arrow.transform.localScale.x, y + arrow.transform.localScale.y, 0), Quaternion.identity);
+                        break;
+                    }
+                case '_': // Normal tile
+                    {
+                        break;
+                    }
+                case 'B': // Battery
+                    {
+                        Instantiate(battery, new Vector3(x - battery.transform.localScale.x, y + battery.transform.localScale.y, 0), Quaternion.identity);
+                        break;
+                    }
+                case 'L': // Lacus
+                    {
+                        Instantiate(lacus, new Vector3(x - lacus.transform.localScale.x, y + lacus.transform.localScale.y, 0), Quaternion.identity);
+                        break;
+                    }
+                case '?': // Button
+                    {
+                        Instantiate(button, new Vector3(x - button.transform.localScale.x, y + button.transform.localScale.y, 0), Quaternion.identity);
+                        break;
+                    }
+                case '!': // Deactivated Arrow
+                    {
+                        Instantiate(arrow, new Vector3(x - arrow.transform.localScale.x, y + arrow.transform.localScale.y, 0), Quaternion.identity);
+                        break;
+                    }
+                case 'F': // Exit
+                    {
+                        Instantiate(final, new Vector3(x - final.transform.localScale.x, y + final.transform.localScale.y, 0), Quaternion.identity);
+                        break;
+                    }
+                case 'S': // Stop
+                    {
+                        Instantiate(stop, new Vector3(x - stop.transform.localScale.x, y + stop.transform.localScale.y, 0), Quaternion.identity);
+                        break;
+                    }
+                case '#': // Wall
+                    {
+                        // Problemes,
+                        // buscar una solució al hardcode de la ultima casella de la fila i la ultima fila,
+                        // es a dir buscar una alternativa a x == 4 i y == 6
+
+                        if (x == 0 && y == 0)
+                        {
+                            // Corner Top Left
+                            Instantiate(TopLeftWall, new Vector3(x - TopLeftWall.transform.localScale.x, y + TopLeftWall.transform.localScale.y, 0), Quaternion.identity);
+                        }
+                        else if (x == 6 && y == 0)
+                        {
+                            // Corner Top Right
+                            Instantiate(TopRightWall, new Vector3(x - TopRightWall.transform.localScale.x, y + TopRightWall.transform.localScale.y, 0), Quaternion.identity);
+                        }
+                        else if (x == 0 && -y == 8) 
+                        {
+                            // Corner Bot Left
+                            Instantiate(BotLeftWall, new Vector3(x - BotLeftWall.transform.localScale.x, y + BotLeftWall.transform.localScale.y, 0), Quaternion.identity);
+                        }
+                        else if (x == 6 && -y == 8)
+                        {
+                            // Corner Bot Right
+                            Instantiate(BotRightWall, new Vector3(x - BotRightWall.transform.localScale.x, y + BotRightWall.transform.localScale.y, 0), Quaternion.identity);
+                        }
+                        else if (y == 0)
+                        {
+                            // Walls top
+                            Instantiate(TopWall, new Vector3(x - TopWall.transform.localScale.x, y + TopWall.transform.localScale.y, 0), Quaternion.identity);
+                        }
+                        else if (y == -8)
+                        {
+                            // Walls Bot
+                            Instantiate(BotWall, new Vector3(x - BotWall.transform.localScale.x, y + BotWall.transform.localScale.y, 0), Quaternion.identity);
+                        }
+                        else if (x == 0)
+                        {
+                            // Walls Left
+                            Instantiate(LeftWall, new Vector3(x - LeftWall.transform.localScale.x, y + LeftWall.transform.localScale.y, 0), Quaternion.identity);
+                        }
+                        else if (x == 6)
+                        {
+                            // Walls Right
+                            Instantiate(RightWall, new Vector3(x - RightWall.transform.localScale.x, y + RightWall.transform.localScale.y, 0), Quaternion.identity);
+                        }
+                        else
+                        {
+                            Debug.LogError("Walls exteriors no correctes");
+                        }
+                        break;
+                    }*/
