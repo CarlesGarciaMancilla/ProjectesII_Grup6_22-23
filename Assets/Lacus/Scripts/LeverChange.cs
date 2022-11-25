54fb1f9966466bf7a12fb426fa98de952fcc019c
Carles Garcia <cgarcia.a3d2n@lamerce.com>
2022-11-25 11:39:33 +0100

    Added more levels
    more levels and a little level change script

---
+++ Assets/Lacus/Scripts/LeverChange.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeverChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            SceneManager.LoadScene("Tutorial_1", LoadSceneMode.Single);
+             
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) 
+        {
+            SceneManager.LoadScene("Tutorial_2", LoadSceneMode.Single);
+             
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("Tutorial_3", LoadSceneMode.Single);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("Tutorial_4", LoadSceneMode.Single);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneManager.LoadScene("Level_1", LoadSceneMode.Single);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            SceneManager.LoadScene("Level_2", LoadSceneMode.Single);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            SceneManager.LoadScene("Level_3", LoadSceneMode.Single);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            SceneManager.LoadScene("Level_4", LoadSceneMode.Single);
+
+        }
+
+
+
+
+    }
+}
