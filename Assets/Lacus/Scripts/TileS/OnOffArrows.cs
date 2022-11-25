1e16581aedaf5136f270fb1814f87901e3dbb3c3
Josep <josep.cusco@enti.cat>
2022-11-25 11:19:31 +0100

    Button Works Arrows do Not

---
+++ Assets/Lacus/Scripts/TileS/OnOffArrows.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffArrows : MonoBehaviour
+{
+    [SerializeField] Button button;
+    [SerializeField] GameObject off;
+    [SerializeField] GameObject fletxa_button;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button.isPressed)
+        {
+            off.SetActive(false);
+            fletxa_button.tag = "Arrow";
+        }
+        else
+        {
+            off.SetActive(true);
+            fletxa_button.tag = "Tile";
+        }
+    }
+}
