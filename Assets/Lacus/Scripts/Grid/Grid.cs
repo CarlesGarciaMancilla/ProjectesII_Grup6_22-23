2deaf43c5534e73b2384495ccb966801a5c4da03
Josep Cuscó <josep.cusco@enti.cat>
2022-12-02 20:29:04 +0100

    gitflow-feature-stash: TilesFix

--- Assets/Lacus/Scripts/Grid/Grid.cs
+++ Assets/Lacus/Scripts/Grid/Grid.cs
@@ -6,7 +6,7 @@ public class Grid : MonoBehaviour
 {
     [SerializeField] private int width;
     [SerializeField] private int height;
-    [SerializeField] private TileSprite tile;
+    [SerializeField] private GameObject tile;
     [SerializeField] private Transform worldCoords; // La camera
 
     private Dictionary<Vector2, TileSprite> tiles;
@@ -21,25 +21,14 @@ public class Grid : MonoBehaviour
         {
             for (int y = -1; y > -height-1; y--)
             {
-                TileSprite spawnedTile = Instantiate(tile, new Vector3(x - leftMargin - x * (1 - tile.transform.localScale.x), y - topMargin - y * (1 - tile.transform.localScale.y)), Quaternion.identity);
+                GameObject spawnedTile = Instantiate(tile, new Vector3(x - leftMargin - x * (1 - tile.transform.localScale.x), y - topMargin - y * (1 - tile.transform.localScale.y)), Quaternion.identity);
                 
                 spawnedTile.name = $"Tile {x} {y}";
 
-                bool isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
-                spawnedTile.Init(isOffset);
-
-                tiles[new Vector2(x - tile.transform.localScale.x, y + tile.transform.localScale.y)] = spawnedTile;            }
+                //tiles[new Vector2(x - tile.transform.localScale.x, y + tile.transform.localScale.y)] = spawnedTile;            
+            }
         }
         worldCoords.transform.position = new Vector3(2, -3, -10);
-    }
-
-    public TileSprite GetTilePosition(Vector2 pos)
-    {
-        if(tiles.TryGetValue(pos, out var tile))
-        {
-            return tile;
-        }
-        return null;
     }
 
     void Start()
