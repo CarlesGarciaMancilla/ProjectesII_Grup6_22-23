8983cbc207b21f2cf57474704abb69009b226493
Carles Garcia <cgarcia.a3d2n@lamerce.com>
2022-12-21 10:11:36 +0100

    mainMenu
    Added the base for the mainMenu

---
+++ Assets/Lacus/Scripts/menujuego.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menujuego : MonoBehaviour
+{
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GestionClickMenu(string menu)
+    {
+        switch (menu)
+        {
+            case "jugar":
+                SceneManager.LoadScene("Preguntas");
+                break;
+            case "exit":
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+               Application.Quit();
+#endif
+
+                break;
+
+
+        }
+
+
+    }
+}
