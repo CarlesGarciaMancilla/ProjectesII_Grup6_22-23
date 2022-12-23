cafb2310af52d7188e584013d8fba370af63d417
m0ki <miquel.forcada@enti.cat>
2022-12-23 16:57:21 +0100

    Fixed Edu's Hotfix

---
+++ Assets/Lacus/Scripts/BatteryFeedback.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryFeedback : MonoBehaviour
+{
+    public GameObject[] bars;
+    public LacusStats stats;
+
+    private void Update()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            bars[i].SetActive(stats.batteryLeft > i);
+        }
+    }
+}
