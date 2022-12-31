c7474825ff0fed966543e9f679c79121d1840f0e
Carles Garcia <cgarcia.a3d2n@lamerce.com>
2022-12-31 13:02:18 +0100

    Included Transitions and early level selector

--- Assets/Lacus/Scripts/menujuego.cs
+++ Assets/Lacus/Scripts/menujuego.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class menujuego : MonoBehaviour
 {
+    [SerializeField] private float transitionTime = 1f;
+
     public GameObject menuPanel;
     public GameObject menuOptions;
+    public GameObject prefabTransition;
     public AudioSource audio;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Transition());
     }
 
     // Update is called once per frame
@@ -24,7 +29,7 @@ public class menujuego : MonoBehaviour
         switch (menu)
         {
             case "play":
-                SceneManager.LoadScene("Tutorial_1");
+                StartCoroutine(SceneLoad());
                 break;
             case "options":
                 menuOptions.SetActive(true);
@@ -56,4 +61,21 @@ public class menujuego : MonoBehaviour
 
 
     }
+
+
+    public IEnumerator SceneLoad()
+    {
+        prefabTransition.SetActive(true);
+        animator.SetTrigger("StartTransition");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene("Tutorial_1");
+    }
+
+    public IEnumerator Transition() 
+    {
+        yield return new WaitForSeconds(transitionTime);
+        prefabTransition.SetActive(false);
+    }
 }
+
+
