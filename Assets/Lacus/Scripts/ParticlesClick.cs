3fb151c75357ba99e75e630acde07aaf8155ae03
Josep <josep.cusco@enti.cat>
2023-03-06 14:41:21 +0100

    Particles appear when and where you click

--- Assets/Lacus/Scripts/ParticlesClick.cs
+++ Assets/Lacus/Scripts/ParticlesClick.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ParticlesClick : MonoBehaviour
 {
     // Start is called before the first frame update
+    public ParticleSystem clickParticles;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePosScreenSpace = Input.mousePosition;
+        Vector2 mousePosWorldSpace = Camera.main.ScreenToWorldPoint(mousePosScreenSpace);
+        transform.position = mousePosWorldSpace;
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickParticles.Play();
+            Debug.Log("Click");
+        }
+
     }
 }
