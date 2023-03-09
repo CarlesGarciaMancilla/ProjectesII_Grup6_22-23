5816da6aa1195f491415e6026f5078fcbb6b5f7a
m0ki <miquel.forcada@enti.cat>
2023-03-09 18:08:52 +0100

    Camera Movement Done
    When Tapping do not move camera not done yet

---
+++ Assets/Lacus/Scripts/Grid/FreeLook.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeLook : MonoBehaviour
+{
+    [SerializeField] private Transform Lacus;
+
+    private Vector3 initialPosition;
+    private Vector3 displacement;
+    private Vector3 cameraPosition;
+
+    private bool isDragging = false;
+
+    void Start()
+    {
+        initialPosition = Lacus.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            displacement = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.transform.position;
+
+            if (!isDragging)
+            {
+                isDragging = true;
+                cameraPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else
+        {
+            isDragging = false;
+        }
+
+        if (isDragging)
+        {
+            Camera.main.transform.position = cameraPosition-displacement;
+        }
+
+        /*if (buttonReset)
+        {
+            Camera.main.transform.position = initialPosition
+        }*/
+    }
+}
